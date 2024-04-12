1f1b726ce820ff05396cff7c8b95902f838e98ae
George <dallge401@gmail.com>
2024-04-12 01:43:33 -0400

    Initial commit of Guess The Number game

---
+++ SEF-A6/A6.cpp
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    int randomNumber = 0;
+    int guess = 0;
+    int numberOfGuesses = 0;
+    int lowerBound = 1, upperBound = 100;
+
+    srand(time(NULL));
+    randomNumber = lowerBound + rand() % (upperBound - lowerBound + 1);
+
+    printf("Guess the Number Game\n");
+    printf("I'm thinking of a number between %d and %d.\n", lowerBound, upperBound);
+
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        numberOfGuesses++;
+
+        if (guess > randomNumber) {
+            printf("Too high! Try again.\n");
+        }
+        else if (guess < randomNumber) {
+            printf("Too low! Try again.\n");
+        }
+        else {
+            printf("Congratulations! You guessed the number in %d attempts!\n", numberOfGuesses);
+        }
+
+    } while (guess != randomNumber);
+
+    return 0;
+
+}
